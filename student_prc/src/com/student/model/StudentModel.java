fc78c6d194432a7213b3e63943fce297625b9344
1719652832 <49515304+1719652832@users.noreply.github.com>
2019-05-16 17:41:00 +0800

    Update StudentModel.java

--- student_prc/src/com/student/model/StudentModel.java
+++ student_prc/src/com/student/model/StudentModel.java
@@ -1,10 +1,10 @@
 package com.student.model;
 
 public class StudentModel {
-	private int id;
-	private String usermane;
-	private String password;
-	private int sex;
-	private String love;
+	private int id;//ID
+	private String usermane;//用户名
+	private String password;//密码
+	private int sex;//性别
+	private String love;//爱好
 
 }
